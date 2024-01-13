9697fb70f06a778a217dd361aeba1cf90e91ed5c
Sushil Gupta <sushi.codepro@gmail.com>
2024-01-13 22:09:58 +0530

    Update: changed About details

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ function About() {
     return (
       <>
         <div className={style.home}>
-          <h1>About Page</h1>
+          <h1>Let's know about Sushil</h1>
         </div>
       </>
     );
