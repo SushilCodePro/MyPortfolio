a202d09da4a3cf4ad6c279e41a3af867c50928de
Sushil Gupta <sushil.codepro@gmail.com>
2024-05-18 20:23:28 +0530

    Project_section

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,9 +2,43 @@ import style from '../style/style.module.css'
 function Projects() {
     return (
       <>
-        <div className={style.items}>
-          <h1>Projects Page</h1>
-        </div>
+        <div className={style.ProContainer}>
+          
+            <div className={style.Project}>
+              <h2>E-Commerce App</h2>
+              <p>React.js, Firebase Store for Real Time Database, Netlify</p>
+              <a
+              href="/kkk"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+                <button className={style.projectButton}>Code Link</button>
+              </a>
+              <a
+              href="/kkk"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+                <button className={style.projectButton}>Live Project</button>
+              </a>
+            </div>
+            <div className={style.Project}>
+              
+            </div>
+            <div className={style.Project}>
+              
+            </div>
+            <div className={style.Project}>
+              
+            </div>
+            <div className={style.Project}>
+              
+            </div>
+            <div className={style.Project}>
+              
+            </div>
+
+          </div>
       </>
     );
   }
